fccba71eff1249ac5398ce27469e30572b434eda
Carlos Alfredo Rico Olvera <42428666+Alfrdorico@users.noreply.github.com>
2023-09-27 12:19:51 -0600

    Primer Ejercicio en C

---
+++ EjercicioPractico1.c
@@ -0,0 +1,10 @@
+int main(){
+    int edad;
+    int numerodeAcciones = 150;
+
+    float AhorrosEnBanco = 10000.40;
+
+    char primeraLetraNombre = 'E';
+    char primeraLetraApellido1 = 'L';
+    char primeraLetraApellido2 ='G';
+}
