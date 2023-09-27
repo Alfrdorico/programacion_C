9e4fea784c8d842e6c793e943ad3d4441226ae74
Carlos Alfredo Rico Olvera <42428666+Alfrdorico@users.noreply.github.com>
2023-09-27 12:23:57 -0600

    Update EjercicioPractico1.c
    Cambiamos las letras de nomnbre y apellido

--- EjercicioPractico1.c
+++ EjercicioPractico1.c
@@ -4,7 +4,7 @@ int main(){
 
     float AhorrosEnBanco = 10000.40;
 
-    char primeraLetraNombre = 'E';
-    char primeraLetraApellido1 = 'L';
-    char primeraLetraApellido2 ='G';
+    char primeraLetraNombre = 'A';
+    char primeraLetraApellido1 = 'R';
+    char primeraLetraApellido2 ='O';
 }
